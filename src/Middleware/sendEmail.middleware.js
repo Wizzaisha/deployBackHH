3d908f5f1a6284372568ea10bc6828151d5df81c
Wizzaisha <ever.ramirez@usantotomas.edu.co>
2022-09-22 21:54:23 -0500

    otro ajuste mas

--- src/Middleware/sendEmail.middleware.js
+++ src/Middleware/sendEmail.middleware.js
@@ -4,39 +4,41 @@ const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 
 
-const {
-    EMAIL_PR,
-    PASSWORD_EMAIL,
-    REFRESH_TOKEN,
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET
-} = process.env;
-
-const myOAuth2Client = new OAuth2(
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
-);
-
-myOAuth2Client.setCredentials({
-    refresh_token: REFRESH_TOKEN
-});
-
-const myAccessToken = myOAuth2Client.getAccessToken();
-
-const transport = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        type: "OAuth2",
-        user: EMAIL_PR,
-        clientId: GOOGLE_CLIENT_ID,
-        clientSecret: GOOGLE_CLIENT_SECRET,
-        refreshToken: REFRESH_TOKEN,
-        accessToken: myAccessToken
-    }
-})
 
 function sendEmail (email, data) {
+    
+    const {
+        EMAIL_PR,
+        PASSWORD_EMAIL,
+        REFRESH_TOKEN,
+        GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET
+    } = process.env;
+
+    const myOAuth2Client = new OAuth2(
+        GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET,
+        "https://developers.google.com/oauthplayground"
+    );
+
+    myOAuth2Client.setCredentials({
+        refresh_token: REFRESH_TOKEN
+    });
+
+    const myAccessToken = myOAuth2Client.getAccessToken();
+
+    const transport = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            type: "OAuth2",
+            user: EMAIL_PR,
+            clientId: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET,
+            refreshToken: REFRESH_TOKEN,
+            accessToken: myAccessToken
+        }
+    })
+
 
     let mailOptions = {
         from: EMAIL_PR,
